95303093839f34f48e19ff3dc480b5539a22c5e3
Maiadee <maiadeemmony@hotmail.co.uk>
2025-02-27 18:08:29 +0000

    talent and user service

---
+++ src/services/talentService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { getToken } from "../utils/auth";
+
+const BASE_URL = import.meta.env.VITE_API_URL + "/talent";
+
+// All talent
+export const talentIndex = async () => {
+  try {
+    const res = await axios.get(BASE_URL);
+    return res.data || null;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// One talent
+export const talentShow = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/${talentId}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
